dc8a6c9cac2eb9cb55a67e4127b18a45c09df3da
Alem911 <77479192309@yandex.kz>
2019-10-17 18:01:59 +0600

    str strbrwaswapad

--- swap.go
+++ swap.go
@@ -1,7 +1,18 @@
 package piscine
 
-func swap(x *int, y *int) {
-	temp = a
-	a = b
-	b = temp
+func swap(px, py *int) {
+	tempx := *px
+	tempy := *py
+	*px = tempy
+	*py = tempx
+}
+func main() {
+	x := int(1)
+	y := int(2)
+	fmt.Println("x was", x)
+	fmt.Println("y was", y)
+	swap(&x, &y)
+
+	fmt.Println("x is now", x)
+	fmt.Println("y is now", y)
 }
